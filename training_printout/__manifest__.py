bf301dac9d6cbeb3ce0dff1ea6bb5b673adbea41
Amirul <muh.amirul.muk@gmail.com>
2023-11-16 20:33:28 +0700

    [ADD] training_printout

---
+++ training_printout/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Custom Report',
+    'version': '16.0',
+    'summary': 'Custom Report',
+    'description': 'Custom Report',
+    'category': 'Productivity',
+    'author': 'Amirul M.',
+    'license': 'LGPL-3',
+    'depends': ['purchase'],
+    'data': [
+        'report/purchase_report.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
